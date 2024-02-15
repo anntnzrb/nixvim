a3dcb5af05d2aa51ac88d5b1c0d1a499d370e3e1
anntnzrb <anntnzrb@proton.me>
2024-02-15 10:47:03 -0500

    modules: wrap in config block

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,39 +1,41 @@
 {
-  plugins = {
-    fugitive.enable = true;
+  config = {
+    plugins = {
+      fugitive.enable = true;
 
-    gitsigns = {
-      enable = true;
+      gitsigns = {
+        enable = true;
 
-      updateDebounce = 500;
-      currentLineBlame = false;
+        updateDebounce = 500;
+        currentLineBlame = false;
+      };
     };
-  };
 
-  keymaps = [
-    {
-      key = "<leader>gg";
-      action = ":Git<CR>";
-      options = {
-        silent = true;
-        desc = "Git: Status";
-      };
-    }
-    {
-      key = "<leader>gp";
-      action = ":Gitsigns preview_hunk<CR>";
-      options = {
-        silent = true;
-        desc = "Git: Preview hunk";
-      };
-    }
-    {
-      key = "<leader>gb";
-      action = ":Gitsigns toggle_current_line_blame<CR>";
-      options = {
-        silent = true;
-        desc = "Git: Toggle current line blame";
-      };
-    }
-  ];
+    keymaps = [
+      {
+        key = "<leader>gg";
+        action = ":Git<CR>";
+        options = {
+          silent = true;
+          desc = "Git: Status";
+        };
+      }
+      {
+        key = "<leader>gp";
+        action = ":Gitsigns preview_hunk<CR>";
+        options = {
+          silent = true;
+          desc = "Git: Preview hunk";
+        };
+      }
+      {
+        key = "<leader>gb";
+        action = ":Gitsigns toggle_current_line_blame<CR>";
+        options = {
+          silent = true;
+          desc = "Git: Toggle current line blame";
+        };
+      }
+    ];
+  };
 }
