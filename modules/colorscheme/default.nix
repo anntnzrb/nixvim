1affac12d6c2aabadf33b6d10c6ecbbd5ff97dc6
anntnzrb <anntnzrb@proton.me>
2024-12-17 19:17:39 -0500

    feat(theme): use dracula

--- modules/colorscheme/default.nix
+++ modules/colorscheme/default.nix
@@ -1,13 +1,17 @@
 {
   config = {
-    colorschemes.rose-pine = {
-      enable = true;
+    colorschemes = {
+      rose-pine = {
+        enable = false;
 
-      settings = {
-        variant = "moon";
-        dark_variant = "moon";
-        dim_inactive_windows = true;
+        settings = {
+          variant = "moon";
+          dark_variant = "moon";
+          dim_inactive_windows = true;
+        };
       };
+
+      dracula-nvim.enable = true;
     };
   };
 }
