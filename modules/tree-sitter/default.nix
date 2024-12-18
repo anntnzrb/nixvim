d4976321ceae33d0608f828d8f0845b02c053f06
anntnzrb <anntnzrb@proton.me>
2024-12-18 00:56:49 -0500

    feat(treesitter): minor tweaks

--- modules/tree-sitter/default.nix
+++ modules/tree-sitter/default.nix
@@ -1,17 +1,33 @@
 {
   config = {
-    plugins = {
-      treesitter = {
-        enable = true;
-        nixvimInjections = true;
+    plugins.treesitter = {
+      enable = true;
+      nixvimInjections = true;
+      folding = false;
 
-        folding = false;
+      settings = {
+        auto_install = true;
+        indent.enable = true;
+        highlight.enable = true;
+        highlight.additional_vim_regex_highlighting = true;
 
-        settings = {
-          auto_install = true;
-          highlight.enable = true;
-          indent.enable = true;
+        incremental_selection = {
+          enable = true;
+          keymaps = {
+            init_selection = "<C-space>";
+            node_incremental = "<C-space>";
+            node_decremental = "<BS>";
+            scope_incremental = false;
+          };
         };
+      };
+    };
+
+    plugins.treesitter-textobjects = {
+      enable = true;
+      select = {
+        enable = true;
+        lookahead = true;
       };
     };
   };
