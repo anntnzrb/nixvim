ef7198c724fb1fe4bdd39f726c3be4572ba65337
anntnzrb <anntnzrb@proton.me>
2024-12-19 17:07:28 -0500

    feat(conform): do not fmt {on,after} save

--- modules/conform/default.nix
+++ modules/conform/default.nix
@@ -1,12 +1,17 @@
 {
+  lib,
+  config,
+  ...
+}:
+{
   config = {
     plugins.conform-nvim = {
       enable = true;
 
       settings = {
         default_format_opts.lsp_format = "fallback";
-        format_after_save.lsp_format = "fallback";
-        format_on_save.lsp_format = "fallback";
+        format_after_save = null;
+        format_on_save = null;
 
         # ft's with no cfg'd fmt
         formatters_by_ft."_" = [
@@ -17,7 +22,7 @@
       };
     };
 
-    keymaps = [
+    keymaps = lib.mkIf config.plugins.conform-nvim.enable [
       {
         action = ":lua require('conform').format({ async = true })<CR>";
         key = "<leader>cf";
