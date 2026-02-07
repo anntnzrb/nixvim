52098adf6eef67bc2ca3501f8d10989f194c92af
anntnzrb <anntnzrb@proton.me>
2026-02-06 22:12:53 -0500

    refactor(lsp): adopt top-level lsp config
    Prefer the current top-level LSP module so keymaps and inlay-hint behavior are expressed in the API nixvim is actively evolving. Avoid legacy plugin-level glue to keep the configuration easier to reason about and less likely to break when compatibility code is removed.

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,18 +1,41 @@
 {
+  lib,
   mkPrefixLeader,
   ...
 }:
 let
   prefixed = mkPrefixLeader "c";
+
+  mkLspBufKeymaps =
+    keymaps:
+    lib.mapAttrsToList (key: lspBufAction: {
+      inherit key lspBufAction;
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Lsp buf ${lspBufAction}";
+      };
+    }) keymaps;
+
+  mkDiagnosticKeymaps =
+    keymaps:
+    lib.mapAttrsToList (key: action: {
+      inherit key;
+      mode = "n";
+      action = lib.nixvim.mkRaw "vim.diagnostic.${action}";
+      options = {
+        silent = true;
+        desc = "Lsp diagnostic ${action}";
+      };
+    }) keymaps;
 in
 {
-  plugins.lsp = {
-    enable = true;
-    inlayHints = true;
+  plugins.lspconfig.enable = true;
 
-    keymaps = {
-      silent = true;
-      lspBuf = prefixed {
+  lsp = {
+    inlayHints.enable = true;
+    keymaps =
+      mkLspBufKeymaps (prefixed {
         d = "definition";
         D = "references";
         i = "implementation";
@@ -21,11 +44,10 @@ in
         r = "rename";
         a = "code_action";
         f = "format";
-      };
-      diagnostic = prefixed {
+      })
+      ++ mkDiagnosticKeymaps (prefixed {
         k = "goto_prev";
         j = "goto_next";
-      };
-    };
+      });
   };
 }
